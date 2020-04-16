1f0c0b89b8cf25c7f4c85ad205ccc21901dcf92a
Hieu.Le <letrunghieu2602@gmail.com>
2020-04-17 00:48:29 +0700

    thực hành

---
+++ LopAnimalvaInterface/src/Animal/Chicken.java
@@ -0,0 +1,13 @@
+package Animal;
+
+import edible.Edible;
+
+public class Chicken extends Animal implements Edible {
+    @Override
+    public String makeSound() {
+        return "Chicken: cluck-cluck";
+    }
+    public String howToEat() {
+        return "could be fried";
+    }
+}
